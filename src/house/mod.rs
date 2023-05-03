c868d69666da3881ce262896fcaf8ff85624e874
e.v.barinov <e.v.barinov@tinkoff.ru>
2023-05-03 11:43:14 +0300

    smart_house-lesson10-hw
    + add_room() fn for impl SmartHouse;
    + unit-tests for house mod;
    + add get_name() and devices() functions for impl Room;
    + unit-tests for room mod;
    + modified code for fn main():

--- src/house/mod.rs
+++ src/house/mod.rs
@@ -1,8 +1,8 @@
+pub mod room;
+
+use crate::house::room::Room;
 use crate::provider::DeviceInfoProvider;
 use std::collections::HashMap;
-use crate::house::room::Room;
-
-pub mod room;
 
 pub struct SmartHouse {
     #[allow(dead_code)]
@@ -12,14 +12,25 @@ pub struct SmartHouse {
 
 impl SmartHouse {
     pub fn new(name: String) -> Self {
-        let room = Room::new(
-            "Room".to_string(),
-            vec!["socket".to_string(), "thermo".to_string()],
-        );
         SmartHouse {
             name,
-            rooms: vec![room],
+            rooms: Vec::new(),
         }
+    }
+
+    pub fn add_room(&mut self, room: Room) -> Option<bool> {
+        let root_names = self
+            .rooms
+            .iter()
+            .map(|r| r.get_name())
+            .collect::<Vec<&str>>();
+
+        if !root_names.contains(&room.get_name()) {
+            self.rooms.push(room);
+            return Some(true);
+        }
+
+        None
     }
 
     pub fn get_rooms(&self) -> [&str; 2] {
@@ -58,5 +69,71 @@ impl SmartHouse {
             .iter()
             .map(|d| d.clone() + "\n")
             .collect::<String>()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::device::SmartThermometer;
+    use crate::provider::BorrowingDeviceInfoProvider;
+    use crate::{
+        device::{SmartSocket, State},
+        provider::OwningDeviceInfoProvider,
+    };
+
+    #[test]
+    fn can_add_room() {
+        let mut house = SmartHouse::new(String::from("smart house"));
+        let room = Room::new(String::from("room"), Vec::new());
+
+        house.add_room(room);
+
+        assert!(house.get_rooms().contains(&"room"));
+    }
+
+    #[test]
+    fn cannot_add_room_with_same_name() {
+        let mut house = SmartHouse::new(String::from("smart house"));
+        let room1 = Room::new(String::from("room"), Vec::new());
+        let room2 = Room::new(String::from("room"), Vec::new());
+
+        house.add_room(room1);
+        house.add_room(room2);
+
+        assert_eq!(house.get_rooms().first(), Some(&"room"));
+        assert_eq!(house.get_rooms().last(), Some(&""));
+    }
+
+    #[test]
+    fn create_report() {
+        let socket = SmartSocket::new(String::from("socket"), State::On);
+        let socket2 = SmartSocket::new(String::from("socket2"), State::Off);
+        let thermo = SmartThermometer::new(String::from("thermo"), "25".to_string());
+        let room1 = Room::new("room".to_string(), vec!["socket".to_string()]);
+        let room2 = Room::new(
+            "room2".to_string(),
+            vec!["thermo".to_string(), "socket2".to_string()],
+        );
+        let mut house = SmartHouse::new(String::from("Smart House:"));
+        house.add_room(room1);
+        house.add_room(room2);
+        let info_provider = OwningDeviceInfoProvider { socket };
+        let info_provider2 = BorrowingDeviceInfoProvider {
+            socket: &socket2,
+            thermo: &thermo,
+        };
+
+        let report1 = house.create_report(&info_provider);
+        let report2 = house.create_report(&info_provider2);
+
+        assert_eq!(
+            report1,
+            "Room: room, Device SmartSocket: socket, state is On\n".to_string()
+        );
+        assert!(report2.contains("Room: room2, Device SmartSocket: socket2, state is Off\n"));
+        assert!(
+            report2.contains("Room: room2, Device Thermometer: thermo, temperature is \"25\"\n")
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,27 @@ mod house;
 mod provider;
 
 use crate::device::{Device, SmartSocket, SmartThermometer, State};
+use crate::house::room::Room;
 use crate::provider::{BorrowingDeviceInfoProvider, OwningDeviceInfoProvider};
 
 fn main() {
-    let house = house::SmartHouse::new(String::from("Smart House"));
-    println!("{:?}", &house.get_rooms());
-    println!("{:?}", &house.devices("Room"));
-
+    let mut house = house::SmartHouse::new(String::from("Smart House"));
     let socket = SmartSocket::new(String::from("socket"), State::On);
     println!("{}", socket.create_report());
-    let socket2 = SmartSocket::new(String::from("socket"), State::Off);
+    let socket2 = SmartSocket::new(String::from("socket2"), State::Off);
     println!("{}", socket2.create_report());
     let thermo = SmartThermometer::new(String::from("thermo"), String::from("25.0"));
+    let room = Room::new(String::from("room"), vec![socket.name.clone()]);
+    let room2 = Room::new(
+        String::from("room2"),
+        vec![socket2.name.clone(), thermo.name.clone()],
+    );
+    house.add_room(room);
+    house.add_room(room2);
+
+    println!("{:?}", &house.get_rooms());
+    println!("{:?}", &house.devices("room"));
+    println!("{:?}", &house.devices("room2"));
     println!("{}", thermo.create_report());
     let info_provider1 = OwningDeviceInfoProvider { socket };
     let info_provider2 = BorrowingDeviceInfoProvider {
